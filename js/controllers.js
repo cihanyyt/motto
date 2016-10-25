532505a1cb10008238605fad75b8b72e35a6e635
Yesilyurt, Cihan (CT DD DS EU TR DF FA-2) <cihan.yesilyurt@siemens.com>
2016-10-25 09:24:30 +0300

    seach forUsername and list uploads

--- js/controllers.js
+++ js/controllers.js
@@ -27,24 +27,48 @@ angular.module('myApp')
 
     $scope.search = function (isNewQuery) {
       $scope.loading = true;
-      $http.get('https://www.googleapis.com/youtube/v3/search', {
+      $http.get('https://www.googleapis.com/youtube/v3/channels', {
         params: {
           key: 'AIzaSyDDheDyEodFf3EPUqMw876deYCoqBIFeoU',
-          type: 'video',
-          maxResults: '10',
+          type: 'channel',
+          maxResults: '50',
           pageToken: isNewQuery ? '' : $scope.nextPageToken,
-          part: 'id,snippet',
-          fields: 'items/id,items/snippet/title,items/snippet/description,items/snippet/thumbnails/default,items/snippet/channelTitle,nextPageToken',
-          q: this.query
+          part: 'id, snippet, contentDetails',
+          forUsername: this.query
         }
       })
       .success( function (data) {
         if (data.items.length === 0) {
           $scope.label = 'No results were found!';
         }
-        VideosService.listResults(data, $scope.nextPageToken && !isNewQuery);
-        $scope.nextPageToken = data.nextPageToken;
-        $log.info(data);
+        else {
+            var item = data.items[0];
+            var _playListId = item.contentDetails.relatedPlaylists.uploads;
+            $http.get('https://www.googleapis.com/youtube/v3/playlistItems', {
+                params: {
+                    key: 'AIzaSyDDheDyEodFf3EPUqMw876deYCoqBIFeoU',
+                    maxResults: '50',
+                    part: 'snippet',
+                    playlistId: _playListId
+                }
+            })
+            .success( function (data) {
+            if (data.items.length === 0) {
+                $scope.label = 'No results were found!';
+            }
+            VideosService.listResults(data, $scope.nextPageToken && !isNewQuery);
+            $scope.nextPageToken = data.nextPageToken;
+            $log.info(data);
+            })
+            .error( function () {
+                $log.info('Search error');
+            })
+            .finally( function () {
+                $scope.loadMoreButton.stopSpin();
+                $scope.loadMoreButton.setDisabled(false);
+                $scope.loading = false;
+            });
+        }
       })
       .error( function () {
         $log.info('Search error');
