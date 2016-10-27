649bda09284cfa5ad7198720841a562d95ae7b95
Yesilyurt, Cihan (CT DD DS EU TR DF FA-2) <cihan.yesilyurt@siemens.com>
2016-10-27 18:18:21 +0300

    avatars for users

--- js/controllers.js
+++ js/controllers.js
@@ -11,22 +11,58 @@ angular.module('myApp')
       $scope.youtube = VideosService.getYoutube();
       $scope.results = VideosService.getResults();
       $scope.history = VideosService.getHistory();
+      $scope.loadd;
     }
 
 
-    $scope.load = function () {
+    $scope.loadd = function () {
         var xmlhttp = new XMLHttpRequest();
         var url = "js/members.json";
 
         xmlhttp.onreadystatechange = function() {
             if (this.readyState == 4 ) {
                 $scope.myArr = JSON.parse(this.responseText);
+                $scope.getAvatars();
             }
         };
         xmlhttp.open("GET", url, true);
         xmlhttp.send();
-    }
+    };
 
+    $scope.getAvatars = function () {
+      var i = 0;
+      while($scope.myArr.length > i) {
+        i++;
+        $http.get('https://www.googleapis.com/youtube/v3/channels', {
+          params: {
+            key: 'AIzaSyDDheDyEodFf3EPUqMw876deYCoqBIFeoU',
+            type: 'channel',
+            maxResults: '50',
+            part: 'id, snippet, contentDetails',
+            forUsername: $scope.myArr[i].id
+          }
+        })
+        .success( function (data) {
+          if (data.items.length === 0) {
+            $scope.label = 'No results were found!';
+          }
+          else {
+            for (var i = $scope.myArr.length - 1; i >= 0; i--) {
+              if($scope.myArr[i].name == data.items[0].snippet.title)
+                  $scope.myArr[i].avatar = data.items[0].snippet.thumbnails.medium.url;
+            }
+          }
+      })
+      .error( function () {
+        $log.info('Search error');
+      })
+      .finally( function () {
+        $scope.loadMoreButton.stopSpin();
+        $scope.loadMoreButton.setDisabled(false);
+        $scope.loading = false;
+      });
+      }
+    };
 
     $scope.launch = function (video, archive) {
       VideosService.launchPlayer(video.id, video.title);
