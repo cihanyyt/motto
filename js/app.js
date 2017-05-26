b2282fe6e5241f662130f81bf8b15670de576ab2
Yesilyurt, Cihan (CT DD DS EU TR PBA SWC 2) <cihan.yesilyurt@siemens.com>
2017-05-26 11:58:45 +0200

    improve publishedAt and styling

--- js/app.js
+++ js/app.js
@@ -43,6 +43,62 @@ app.service('VideosService', ['$window', '$rootScope', '$log', function ($window
     $rootScope.$apply();
   };
 
+  this.getElapsed = function(publishedAt){
+
+      var today = new Date();
+      var dd = today.getDate();
+      var mm = today.getMonth()+1; //January is 0!
+      var yyyy = today.getFullYear();
+
+      if(dd < 10) {
+          dd='0'+ dd
+      }
+      if(mm < 10) {
+          mm='0'+ mm
+      }
+
+      //today = mm+'/'+dd+'/'+yyyy;
+
+      var publishedYear = publishedAt.toString().slice(0,4);
+      var publishedMonth = publishedAt.toString().slice(5,7);
+      var publishedDay = publishedAt.toString().slice(8,10);
+
+      var asd = publishedYear.toString() + " " + publishedMonth.toString() + " " + publishedDay.toString();
+
+      var elapsedYear = yyyy - publishedYear;
+      var elapsedMonth = mm - publishedMonth;
+      var elapsedDay = dd - publishedDay;
+
+
+      if (elapsedYear > 0)
+      {
+          if (elapsedYear == 1)
+              asd = "last year";
+          else
+              asd = elapsedYear + " years ago"
+      }
+      else if (mm - publishedMonth>0)
+      {
+          if (elapsedMonth == 1)
+              asd = "last month";
+          else
+              asd = elapsedMonth + " months ago"
+      }
+      else if (dd - publishedDay > 0)
+      {
+          if (elapsedDay == 1)
+              asd = "yesterday";
+          else
+              asd = elapsedDay + " days ago"
+      }
+      else
+      {
+          asd = "some hours ago"
+      }
+
+      return asd;
+  }
+
   this.bindPlayer = function (elementId) {
     $log.info('Binding to ' + elementId);
     youtube.playerId = elementId;
@@ -83,11 +139,12 @@ app.service('VideosService', ['$window', '$rootScope', '$log', function ($window
     for (var i = 0 ; i < data.items.length; i++) {
         if(data.items[i].kind == "youtube#searchResult" ) {
             if(data.items[i].id.kind == "youtube#video" ) {
+
                 results.push({
                     id: data.items[i].id.videoId,
                     title: data.items[i].snippet.title,
                     description: data.items[i].snippet.description,
-                    publishedAt: data.items[i].snippet.publishedAt,
+                    publishedAt: this.getElapsed(data.items[i].snippet.publishedAt),
                     thumbnail: data.items[i].snippet.thumbnails.medium.url,
                     author: data.items[i].snippet.channelTitle
                 });
@@ -98,7 +155,7 @@ app.service('VideosService', ['$window', '$rootScope', '$log', function ($window
                 id: data.items[i].contentDetails.videoId,
                 title: data.items[i].snippet.title,
                 description: data.items[i].snippet.description,
-                publishedAt: data.items[i].snippet.publishedAt,
+                publishedAt: this.getElapsed(data.items[i].snippet.publishedAt),
                 thumbnail: data.items[i].snippet.thumbnails.medium.url,
                 author: data.items[i].snippet.channelTitle
             });
@@ -108,7 +165,7 @@ app.service('VideosService', ['$window', '$rootScope', '$log', function ($window
                 id: data.items[i].contentDetails.videoId,
                 title: data.items[i].snippet.title,
                 description: data.items[i].snippet.description,
-                publishedAt: data.items[i].snippet.publishedAt,
+                publishedAt: this.getElapsed(data.items[i].snippet.publishedAt),
                 thumbnail: data.items[i].snippet.thumbnails.medium.url,
                 author: data.items[i].snippet.channelTitle
             });
